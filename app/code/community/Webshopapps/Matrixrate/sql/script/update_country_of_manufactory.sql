1092308212b352a84f17abbeea1a7ec590544bdf
Lei Shang <shang.lei@icloud.com>
2016-03-15 18:11:17 +0100

    adding queries for weight & country of manufactory

--- app/code/community/Webshopapps/Matrixrate/sql/script/update_country_of_manufactory.sql
+++ app/code/community/Webshopapps/Matrixrate/sql/script/update_country_of_manufactory.sql
@@ -8,3 +8,10 @@ SET
     pev.value = 'DE'
 WHERE
     pev.entity_id = '<#the product id to update>'
+
+
+# check if there is any product doesn't have value for country_of_manufacture
+select * from catalog_product_entity_varchar AS pev JOIN eav_attribute AS ea ON ea.entity_type_id = 4
+    AND ea.attribute_code = 'country_of_manufacture'
+    AND pev.attribute_id = ea.attribute_id
+    where pev.value = ''    
--- app/code/community/Webshopapps/Matrixrate/sql/script/update_product_weight.sql
+++ app/code/community/Webshopapps/Matrixrate/sql/script/update_product_weight.sql
@@ -10,3 +10,9 @@ SET
     ped.value = {your new value}
 WHERE
     ped.entity_id = {the product id to update}
+
+#check if there is any product's weight is unset
+select * from catalog_product_entity_decimal AS ped JOIN eav_attribute AS ea ON ea.entity_type_id = 4
+    AND ea.attribute_code = 'weight'
+    AND ped.attribute_id = ea.attribute_id
+    where ped.value = 0    
